9e6c2cfaa21dd3b42fd2ec1a500d22f6eacade07
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-08-11 11:15:45 +1000

    Update update-available.js

--- nitro-friends/update-available.js
+++ nitro-friends/update-available.js
@@ -24,11 +24,12 @@ async function displayData() {
     if (!!params["apple-app-store"]) {
         if (params["apple-app-store"] == 1 || params["apple-app-store"] == true) {
             download.href = "https://apps.apple.com/app/id1573370632";
+            ours.innerHTML = data.version_check_apple[data.version_check_apple.length - 1];
             apple_app_store = true;
         }
     }
-    ours.innerHTML = data.version_check[data.version_check.length - 1];
-    if (!apple_app_store) {
+        if (!apple_app_store) {
+        ours.innerHTML = data.version_check[data.version_check.length - 1];
         download.href = "https://github.com/JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET/nitroFriends/releases/tag/" + data.version_check[data.version_check.length - 1];
     }
     download.hidden = false;
