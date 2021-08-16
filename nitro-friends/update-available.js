6a693802f0869b237c8c7af94cf404bf5a949089
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-08-16 14:09:20 +1000

    Update update-available.js

--- nitro-friends/update-available.js
+++ nitro-friends/update-available.js
@@ -7,7 +7,7 @@ const changelog = document.getElementById("changelogs");
 const changelogText = changelog.getElementsByTagName("p")[0];
 
 var data;
-var apple_app_store;
+var store;
 var changelogData = "";
 
 async function getVersions() {
@@ -29,34 +29,44 @@ async function displayData() {
         if (params["apple-app-store"] == 1 || params["apple-app-store"] == "true") {
             download.href = "https://apps.apple.com/app/id1573370632";
             ours.innerHTML = data.version_check_apple[data.version_check_apple.length - 1];
-            apple_app_store = true;
+            store = "apple";
+        }
+    } else if (!!params["chrome-web-store"]) {
+        if (params["chrome-web-store"] == 1 || params["chrome-web-store"] == "true") {
+            download.href = "https://josephyapyeeeeeeeeeeeeeeeeeeeeeeeeeeeet.github.io/nitro-friends/not-live";
+            ours.innerHTML = data.version_check_google[data.version_check_google.length - 1];
+            store = "chrome";
         }
     }
-        if (!apple_app_store) {
+        if (!(store == "apple") && !(store == "chrome")) {
         ours.innerHTML = data.version_check[data.version_check.length - 1];
         download.href = "https://github.com/JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET/nitroFriends/releases/tag/" + data.version_check[data.version_check.length - 1];
     }
     download.hidden = false;
-    displayChangelogs(apple_app_store, false);
+    displayChangelogs(store, false);
 }
 
 
-async function displayChangelogs(apple, force) {
+async function displayChangelogs(store, force) {
     var foundVersion = false;
     if (force) {
-        if (!apple) {
+        if (!(store == "apple") && !(store == "chrome")) {
             for (let i = 0; i < data.version_check.length; i++) {
                 changelogData = `${changelogData}${data.changelogs[data.version_check[i]]}</br>`;
                 foundVersion = true;
             }
-        } else {
+        } else if (store == "apple") {
             for (let i = 0; i < data.version_check_apple.length; i++) {
                 changelogData = `${changelogData}${data.changelogs_apple[data.version_check_apple[i]]}</br>`;
                 foundVersion = true;
             }
+        } else {
+            for (let i = 0; i < data.version_check_google.length; i++) {
+                changelogData = `${changelogData}${data.changelogs_google[data.version_check_google[i]]}</br>`;
+                foundVersion = true;
         }
     } else {
-        if (!apple) {
+        if (!(store == "apple") && !(store == "chrome")) {
             for (let i = 0; i < data.version_check.length; i++) {
                 if (foundVersion) {
                     changelogData = `${changelogData}${data.changelogs[data.version_check[i]]}</br>`;
@@ -65,12 +75,21 @@ async function displayChangelogs(apple, force) {
                     foundVersion = true;
                 }
             }
-        } else {
+        } else if (store == "apple"){
             for (let i = 0; i < data.version_check_apple.length; i++) {
                 if (foundVersion) {
                     changelogData = `${changelogData}${data.changelogs_apple[data.version_check_apple[i]]}</br>`;
                 }
                 if (data.version_check_apple[i] == params.version) {
+                    foundVersion = true;
+                }
+            }
+        } else {
+            for (let i = 0; i < data.version_check_google.length; i++) {
+                if (foundVersion) {
+                    changelogData = `${changelogData}${data.changelogs_google[data.version_check_google[i]]}</br>`;
+                }
+                if (data.version_check_google[i] == params.version) {
                     foundVersion = true;
                 }
             }
